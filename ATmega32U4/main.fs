74d494b791b6e9b0cb6ed66c933ff6a1c0ab7f79
CharleyShattuck <cshattuck@surewest.net>
2015-04-12 16:16:59 -0700

    first time downloading to 32U4 using myforth-arduino

--- ATmega32U4/main.fs
+++ ATmega32U4/main.fs
@@ -3,5 +3,9 @@
 \ This is where your application goes.
 \ It should have a main loop called "go". 
 
-: go  go ;
+: ms ( n)  for 4000 # for next next ;
+: go  7 DDRC sbi,   \ pin 13 is an output
+   begin  7 PORTC sbi,  500 # ms
+          7 PORTC cbi,  500 # ms
+   again
 
