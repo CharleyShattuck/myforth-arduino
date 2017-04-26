ea1361471071cddc511a30d18fb913bd9833d64d
CharleyShattuck <cshattuck@surewest.net>
2017-04-26 14:35:00 -0700

    GNU lesser GPL lice license message added to main.fs

--- stenomod12a/main.fs
+++ stenomod12a/main.fs
@@ -1,5 +1,27 @@
 \ main.fs  modified for extra S- key
 
+0 [if]
+Copyright (C) 2016-2017 by Charles Shattuck.
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+For LGPL information:   http://www.gnu.org/copyleft/lesser.txt
+
+[then]
+
+
 \ wired to make TX Bolt protocol easy,
 \ can read six bits at a time in correct order
 
